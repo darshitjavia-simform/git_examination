331f411a00ad2f5fe63adf089199dd86ce4b4825
Darshit javia <darshit.javia@simformsolutions.com>
2025-03-17 11:28:14 +0530

    Initial commit for project

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
